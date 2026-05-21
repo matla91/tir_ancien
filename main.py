7c275d06b16ab1f0c591ec6ad23be0d29847e11f
Matthis LAHARGOUE <133364346+matla91@users.noreply.github.com>
2026-05-21 13:10:06 -0400

    feat: enable toggle breath control

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.game import Game
+from src.breath_toggle_patch import apply_breath_toggle_patch
 
 
 def main() -> None:
+    apply_breath_toggle_patch(Game)
     game = Game()
     game.run()
 
